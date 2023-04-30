254dbc02fda0811717164021778acb1fc6ffedaa
charles <wkc1002@naver.com>
2023-05-01 01:32:20 +0900

    서블릿, HttpServletRequest

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,25 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8"); // 요즘에는 웬만하면 문자 인코딩은 utf-8로 써야한다.
+        response.getWriter().write("hello " + username); // HTTP Message Body에 값을 입력해준다.
+    }
+}
