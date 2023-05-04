1665415e7177b72d89d36b379735e3881a7a1df5
charles <wkc1002@naver.com>
2023-05-04 16:44:16 +0900

    MVC 프레임워크 만들기 - 유연한 컨트롤러2 V5

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -5,7 +5,11 @@ import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -36,10 +40,16 @@ public class FrontControllerServletV5 extends HttpServlet {
         handelrMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handelrMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handelrMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        // V4 추가
+        handelrMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handelrMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handelrMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     @Override
@@ -58,6 +68,7 @@ public class FrontControllerServletV5 extends HttpServlet {
 
         String viewName = mv.getViewName(); // 논리이름
         MyView view = viewResolver(viewName);
+
         view.render(mv.getModel(), request, response);
     }
 
