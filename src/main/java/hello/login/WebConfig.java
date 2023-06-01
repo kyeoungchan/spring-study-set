ba3c6b513d2ff7a1593c3c4659fe0266169378ed
charles <wkc1002@naver.com>
2023-06-01 17:21:21 +0900

    temp

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -2,16 +2,28 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 이유가 있어서라기 보다는 스프링이 이 방식으로 제공하기 때문에 이거를 따르면 된다.
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error"); // 인터셉터를 먹이지 않을 path들
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         /* 스프링 부트가 WAS를 들고 띄우기 때문에 WAS를 띄울 때 필터를 스프링부트가 같이 띄워준다.*/
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
