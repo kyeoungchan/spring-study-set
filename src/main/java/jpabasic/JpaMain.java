5ae0c4e4552938757d2392cac1da9fe1e71ba1e7
charles <wkc1002@naver.com>
2023-06-14 15:36:58 +0900

    Eager Loading, Lazy Loading, Casacade, OrphanRemoval

--- src/main/java/jpabasic/JpaMain.java
+++ src/main/java/jpabasic/JpaMain.java
@@ -3,6 +3,7 @@ package jpabasic;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,26 +17,20 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1  = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            em.persist(member1);
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member1.getId());
-
-            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
-
-            System.out.println("===============");
-            m.getTeam().getName(); // Team에 있는 무언가를 터치한 시점에 쿼리가 나가게 된다.
-            System.out.println("===============");
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
 
             tx.commit();
         } catch (Exception e) {
