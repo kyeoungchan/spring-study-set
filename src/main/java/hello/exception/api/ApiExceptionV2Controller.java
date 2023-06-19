10ac5125915509c02c1f270c75d634beca3fb972
charles <wkc1002@naver.com>
2023-06-19 15:24:42 +0900

    ExceptionHandlerExceptionResolver 2

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,16 +1,40 @@
 package hello.exception.api;
 
 import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class ApiExceptionV2Controller {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage()); // 해당 객체가 그대로 json으로 반환이 된다.
+    }
+
+    //    @ExceptionHandler(UserException.class) // 메서드 파라미터로 Exception에 대한 정보가 있을 시 애노테이션 파라미터 생략 가능
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) { // Exception이 제일 최상위 예외이므로 위에 IllegalArgumentException이나 UserException이 해결하지 못한 예외들은 여기서 해결된다.
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
 
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
