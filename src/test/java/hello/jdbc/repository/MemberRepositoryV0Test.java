ff733200259f26e8fbcbed7c6c1e35c9d6dcc5f6
charles <wkc1002@naver.com>
2023-06-27 17:43:34 +0900

    inquiry

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,20 +1,30 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
-    void crud() throws SQLException { // save
+    void crud() throws SQLException {
         // repository.save에서 checked Exception이 올라오기 때문에 처리를 해야한다.
 
-        Member member = new Member("memberV0", 10000);
+        // save
+        Member member = new Member("memberV1", 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
