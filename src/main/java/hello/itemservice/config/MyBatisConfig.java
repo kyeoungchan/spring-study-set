29cb96395915df267a50de1f161c060d3e887889
charles <wkc1002@naver.com>
2023-07-12 16:31:15 +0900

    MyBatis Application 2 - Settings and Excecute

---
+++ src/main/java/hello/itemservice/config/MyBatisConfig.java
@@ -0,0 +1,29 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.mybatis.ItemMapper;
+import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class MyBatisConfig {
+
+    private final ItemMapper itemMapper;
+    // MyBatis 모듈이 DataSource와 트랜잭션 매니저를 다 읽어와서 ItemMapper와 연결시켜준다.
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new MyBatisItemRepository(itemMapper);
+    }
+
+}
