c0f1b37b04c2306c6dcb214a02ff9ea651470d78
charles <wkc1002@naver.com>
2023-07-18 19:34:42 +0900

    Pratical Structure

---
+++ src/main/java/hello/itemservice/config/V2Config.java
@@ -0,0 +1,38 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV3;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class V2Config {
+
+    private final EntityManager em;
+    private final ItemRepositoryV2 itemRepositoryV2; // SpringDataJPA
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV2(itemRepositoryV2, itemQueryRepositoryV2());
+    }
+
+    @Bean
+    public ItemQueryRepositoryV2 itemQueryRepositoryV2() {
+        return new ItemQueryRepositoryV2(em);
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        // 원래 이번 예제에서는 쓰지 않을 예정이었으나 테스트 케이스를 작동시키기 위해 주입시킨다.
+        return new JpaItemRepositoryV3(em);
+    }
+
+}
