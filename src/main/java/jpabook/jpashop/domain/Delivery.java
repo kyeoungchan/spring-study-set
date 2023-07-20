2021227ba924c692cfdadd28b4cd147a257b6068
charles <wkc1002@naver.com>
2023-07-20 14:47:53 +0900

    Entity Class Coding

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // Enum을 사용할 때는 꼭 EnumType.STRING을 지정해줘야한다.
+    private DeliveryStatus status; // READY, COMP
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 전략을 부모 클래스에서 꼭 잡아줘야 한다.
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+}
