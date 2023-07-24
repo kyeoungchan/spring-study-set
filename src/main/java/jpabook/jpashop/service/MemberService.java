ba9b96175ad102489fbb190c7629e22338fbc0be
charles <wkc1002@naver.com>
2023-07-24 12:18:59 +0900

    Member Repository, Member Service Develop

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // JPA가 성능을 더 최적화한다. flush를 안 해서 더티체킹을 안 하거나, 데이터베이스에게 읽기 전용으로 읽어라고 하는 드라이버도 있고 등
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // readOnly = false 적용
+    public Long join(Member member) {
+        validateDuplicatedMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        /* 실무에서는 최후의 방어로 멀티 스레드 상황 고려해서 DB 에서 Member 의 name 을 unique 제약 조건으로 잡아준다.*/
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
