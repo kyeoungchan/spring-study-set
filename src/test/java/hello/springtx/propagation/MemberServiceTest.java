9acc7dbeabfed05fd9c56ecb8a6e514735b2d280
charles <wkc1002@naver.com>
2023-07-25 15:58:40 +0900

    Transaction Propagation - Single Transaction

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -54,9 +54,28 @@ class MemberServiceTest {
                 .isInstanceOf(RuntimeException.class);
 
 
-        // then : 모든 데이터가 정상 저장된다.
+        // then : 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isEmpty());
+
+    }
+
+    /**
+     * memberService     @Transactional:ON
+     * memberRepository  @Transactional:OFF
+     * logRepository     @Transactional:OFF
+     */
+    @Test
+    void singleTx() {
+        // given
+        String username = "outerTxOff_success";
+
+        // when
+        memberService.joinV1(username);
+
+        // then : 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
 
     }
 
