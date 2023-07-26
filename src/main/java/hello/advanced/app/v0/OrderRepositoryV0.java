37e4226b0ae7672515de5065b39d4941e7c34035
charles <wkc1002@naver.com>
2023-07-26 16:41:29 +0900

    create v0 project

--- src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -7,5 +7,19 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV0 {
 
-    public
+    public void save(String itemId) {
+        // 저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
