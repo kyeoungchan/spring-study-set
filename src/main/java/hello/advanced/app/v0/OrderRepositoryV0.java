214b8600d3c607714e66f3a3d725b8e993e0661b
charles <wkc1002@naver.com>
2023-07-26 16:29:15 +0900

    first commit

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,11 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public
+}
