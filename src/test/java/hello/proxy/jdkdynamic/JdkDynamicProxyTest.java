c15c50e46cfea930b72ead06379d72ad6dd266bc
charles <wkc1002@naver.com>
2023-08-12 15:41:33 +0900

    JDK Dynamic Proxy Example

---
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,39 @@
+package hello.proxy.jdkdynamic;
+
+import hello.proxy.jdkdynamic.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+
+    @Test
+    void dynamicA() {
+        AInterface target = new AImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        // JDK 에서 생성해주는 프록시
+        // 첫 번째 인자 : 프록시가 어디에 생성될지 지정해준다.
+        // 두 번째 인자 : 프록시 기반이 되는 인터페이스. 인터페이스가 여러 개일 수 있으므로 배열이다.
+        // 세 번째 인자 : 이 프록시가 사용될 로직
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BInterface target = new BImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+}
