e3e11af42ee7c9e673677704dabe2261ae8f18a0
charles <wkc1002@naver.com>
2023-08-12 16:48:08 +0900

    CGLIB

---
+++ src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
@@ -0,0 +1,31 @@
+package hello.proxy.cglib.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeMethodInterceptor implements MethodInterceptor {
+
+    private final Object target;
+
+    public TimeMethodInterceptor(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = methodProxy.invoke(target, args);
+//        Object result = method.invoke(target, args); // 이렇게 써도 되지만 CGLIB 는 methodProxy가 더 빠르므로 사용하는 것을 권장한다.
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
