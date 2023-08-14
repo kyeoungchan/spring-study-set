2b1da415eaf4573259df8dab10d8de94aaa94535
charles <wkc1002@naver.com>
2023-08-14 18:07:55 +0900

    ProxyFactory Example

--- src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
+++ src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
@@ -21,7 +21,7 @@ public class TimeMethodInterceptor implements MethodInterceptor {
         long startTime = System.currentTimeMillis();
 
         Object result = methodProxy.invoke(target, args);
-//        Object result = method.invoke(target, args); // 이렇게 써도 되지만 CGLIB 는 methodProxy가 더 빠르므로 사용하는 것을 권장한다.
+//        Object result = method.invoke(target, args); // 이렇게 써도 되지만 CGLIB 는 methodProxy 가 더 빠르므로 사용하는 것을 권장한다.
 
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
---
+++ src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,22 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+//        Object result = method.invoke(target, args);
+        Object result = invocation.proceed(); // 여기서 알아서 타겟을 찾아서 실제 메서드 호출을 해준다.
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
