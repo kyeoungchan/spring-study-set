f0f55765799b28c268bae3eb7471feb695dbc824
charles <wkc1002@naver.com>
2023-08-26 20:10:51 +0900

    약간 수정

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -14,10 +14,16 @@ public class OrderQueryRepository {
 
     private final EntityManager em;
 
-
+    /**
+     * 컬렉션은 별도로 조회
+     * Query : 루트 1번, 컬렉션 N번
+     * 단 건 조회에서 많이 사용하는 방식
+     */
     public List<OrderQueryDto> findOrderQueryDtos() {
+        // 루트 조회(toOne 코드를 모두 한 번에 조회)
         List<OrderQueryDto> result = findOrders(); // Query 1번 -> N개
 
+        // 루프를 돌면서 컬렉션 추가(추가 쿼리 실행)
         result.forEach(o -> {
             List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId()); // Query N번
             o.setOrderItems(orderItems);
@@ -25,14 +31,29 @@ public class OrderQueryRepository {
         return result;
     }
 
+    /**
+     * 최적화
+     * Query : 루트 1번, 컬렉션 1번
+     * 데이터를 한꺼번에 처리할 때 많이 사용하는 방식
+     */
     public List<OrderQueryDto> findAllByDto_optimization() {
+        // 루트 조회(toOne 코드를 모두 한 번에 조회)
         List<OrderQueryDto> result = findOrders();
 
+        // orderItem 컬렉션을 MAP 한 방에 조회
         Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
 
+        // 루프를 돌면서 컬렉션 추가(추가 쿼리 실행X)
         result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
 
         return result;
+    }
+
+    private static List<Long> toOrderIds(List<OrderQueryDto> result) {
+        List<Long> orderIds = result.stream()
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+        return orderIds;
     }
 
     private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
@@ -50,13 +71,22 @@ public class OrderQueryRepository {
         return orderItemMap;
     }
 
-    private static List<Long> toOrderIds(List<OrderQueryDto> result) {
-        List<Long> orderIds = result.stream()
-                .map(o -> o.getOrderId())
-                .collect(Collectors.toList());
-        return orderIds;
+    /**
+     * 1:N 관계(컬렉션)를 제외한 나머지를 한 번에 조회
+     * @return
+     */
+    private List<OrderQueryDto> findOrders() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderQueryDto.class)
+                .getResultList();
     }
 
+    /**
+     * 1:N 관계인 orderItems 조회
+     */
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
         return em.createQuery(
                         "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
@@ -64,15 +94,6 @@ public class OrderQueryRepository {
                                 " join oi.item i" +
                                 " where oi.order.id = :orderId", OrderItemQueryDto.class)
                 .setParameter("orderId", orderId)
-                .getResultList();
-    }
-
-    private List<OrderQueryDto> findOrders() {
-        return em.createQuery(
-                        "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
-                                " from Order o" +
-                                " join o.member m" +
-                                " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
     }
 
