77e1aef6088d8a543840e1080c740b8a6feff757
charles <wkc1002@naver.com>
2023-08-31 17:32:16 +0900

    Query Method - JPA Named Query

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -9,6 +11,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
     List<Member> findHelloBy();
-
     List<Member> findTop3HelloBy();
+
+    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username); // 메서드 명은 자유롭게 짜도 된다.
 }
