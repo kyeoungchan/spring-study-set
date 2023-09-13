6ddce9bdeec1d02879adee130d31a0831322fc51
charles <wkc1002@naver.com>
2023-09-13 18:45:07 +0900

    확장 기능 - Web 확장 - 도메인 클래스 컨버터

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,34 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        // 스프링이 컨버팅하는 과정을 다 해줘서 Member 객체를 바로 인젝션 해준다.
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
