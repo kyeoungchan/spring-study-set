40d3e4cb6ec04ae880e930baa768f9118d6b2bc3
charles <wkc1002@naver.com>
2023-09-13 15:51:51 +0900

    확장 기능 - 사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -320,4 +320,9 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
 
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
+
 }
